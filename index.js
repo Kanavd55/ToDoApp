656108b62b631512238a2c412a3f0bf8f3c914c9
unknown <kanavdahat55@gmail.com>
2022-09-15 03:13:51 +0530

    added ejs and view engine

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express=require('express');
 const port=8000;
 const app=express();
 
+app.use(express.urlencoded());
+app.use(express.static('static'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.get('/',function(req,res){
+    console.log(req.body);
+    return res.render('home',{title:"my app"});
+})
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error is ${err}`);
